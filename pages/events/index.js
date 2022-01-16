01a67c8b4d9478775bccdbc9f682bd526ac9eb10
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-01-16 12:30:06 -0800

    added dummy data, components and styling

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,4 +1,9 @@
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/EventList";
+
 function AllEventsPage() {
+    const events = getAllEvents();
+
     return (
         <div>
             <h1>All Events</h1>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/EventList';
+
 function HomePage () {
+  const featuredEvents = getFeaturedEvents();
+
   return (
   <div>
     <h1>HomePage</h1>
+    <EventList items={featuredEvents} />
   </div>
   );
 }
