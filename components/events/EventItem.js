459e6ff3d60e8372824218105616365b58eccc97
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-01-17 10:28:50 -0800

    added routes and styling

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -13,7 +13,7 @@ function EventItem(props) {
     year: "numeric",
   });
 
-  const formattedAddress = location.replace(", ", "\n");
+  const formattedAddress = location.replace(', ', '\n');
   const exploreLink = `/events/${id}`;
 
   return (
--- components/events/EventList.js
+++ components/events/EventList.js
@@ -9,6 +9,7 @@ function EventList(props) {
       {items.map((event) => (
         <EventItem
           key={event.id}
+          id={event.id}
           title={event.title}
           location={event.location}
           date={event.date}
